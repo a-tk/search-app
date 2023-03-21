71ccdc22413540188a017965d0da29029a989a8a
Andre Keys <andrekeys@u.boisestate.edu>
2023-03-21 11:41:15 -0600

    Dependency injection for testing

--- backend/src/api/api-service.js
+++ backend/src/api/api-service.js
@@ -1,15 +1,23 @@
 const axios = require('axios');
 
-const apiService = ((log4js) => {
+const apiService = ((log4js, url) => {
   const log = log4js.getLogger('api-service');
+  let wikiUrl;
+
+  if (!url) {
+    wikiUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=info|extracts&exintro&explaintext&inprop=url&format=json&titles=';
+  } else {
+    wikiUrl = url;
+  }
+
+  log.info(`Wiki api being used is ${wikiUrl}`);
 
   const search = async (keyword) => {
     log.info(`search called with ${keyword}`);
     let data;
 
-    // https://en.wikipedia.org/w/api.php?action=query&prop=info|extracts&exintro&explaintext&titles=test&format=json&inprop=url
     try {
-      data = await axios.get(`https://en.wikipedia.org/w/api.php?action=query&prop=info|extracts&exintro&explaintext&inprop=url&format=json&titles=${keyword}`);
+      data = await axios.get(wikiUrl + keyword);
     } catch (err) {
       log.error(`encountered ${err}`);
     }
